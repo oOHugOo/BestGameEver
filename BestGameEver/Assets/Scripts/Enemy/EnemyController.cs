e9d116dfcfe3bdf66e7b1eb6428f6dcc63b927d1
Hugo <hugo.moreno@hotmail.ch>
2019-01-05 13:43:25 +0100

    Skeleton Animation now follows the direction!

---
+++ BestGameEver/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Animator anim;
+    Vector3 lastPosition= Vector3.zero;
+    Vector3 speed;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+        lastPosition = transform.position;
+        
+   
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = (transform.position - lastPosition);
+        lastPosition = transform.position;
+        //Debug.Log("speedX = "+speed.x + " /speedY = " +speed.y +"/ speedZ = "+speed.z );
+        
+   
+
+        anim.SetFloat("SpeedX", speed.x);
+        anim.SetFloat("SpeedY", speed.y);
+    }
+}
